d0731a70e3c50ddea7dbbf858ed8ad4892c520ed
JoshuaPaino <joshua.paino@gmail.com>
2022-10-24 17:53:43 -0500

    Start comp_api

---
+++ vue3_CompositionApi/src/assets/YummyMeal.vue
@@ -0,0 +1,32 @@
+<template lang="">
+    <div>
+        <p>
+            <strong>{{name}} </strong>
+            {{ pricePretty }}
+            <button @click="addToCart">Add To Cart</button>
+        </p>
+    </div>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+    props: {
+        name: String,
+        price: Number,
+    },
+    setup(props, { emit }) {
+        const addToCart = () => emit("AddToCart", props.name);
+        const pricePretty = computed(() => `$${props.price}`);
+
+        return { addToCart, pricePretty };
+
+    },
+
+}
+</script>
+
+<style lang="">
+    
+</style>
