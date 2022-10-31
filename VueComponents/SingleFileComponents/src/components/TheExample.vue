359d8ac4b45d17f895889aff11a24036b3bd1986
JoshuaPaino <joshua.paino@gmail.com>
2022-10-31 11:29:11 -0500

    Finish SingleFileComponent Project

---
+++ VueComponents/SingleFileComponents/src/components/TheExample.vue
@@ -0,0 +1,25 @@
+<template lang="">
+   <p class="User">{{User}}</p>
+</template>
+
+<script>
+
+export default {
+    props:{
+        User: {
+            type:String,
+            required: true,
+        }
+    }
+
+}
+</script>
+
+<style lang="css" scoped>
+
+
+.User {
+    color: yellow;
+    font-size: 50px;
+}
+</style>
